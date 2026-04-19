0bdc4e226c532c876aea47f5fd178e59cb8f27a8
Mishan9 <hello1_95@mail.ru>
2026-04-19 20:27:49 +0300

    обавлен TextBoxTest с Allure скриншотами

--- src/test/java/test/my/TextBoxTest.java
+++ src/test/java/test/my/TextBoxTest.java
@@ -1,49 +1,40 @@
 package test.my;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
-import io.qameta.allure.Allure;
 import io.qameta.allure.Owner;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import test.page.Page.TestBox;
-import io.qameta.allure.Step;
-
-import static com.codeborne.selenide.Selenide.*;
-import static io.qameta.allure.Allure.attachment;
-
 public class TextBoxTest {
-    TestBox  page = new TestBox ();
+
+    TestBox page = new TestBox();
     Faker faker = new Faker();
 
-
     @BeforeAll
-     static void  setup ()
-    {  SelenideLogger.addListener("allure", new AllureSelenide());
-       Configuration.browserSize = "1980x2890";
+    static void setup() {
+        // Подключаем Allure Selenide listener
+        SelenideLogger.addListener("AllureSelenide",
+                new AllureSelenide()
+                        .screenshots(true)
+                        .savePageSource(true)
+        );
 
-
+        Configuration.browserSize = "1920x1080";
     }
 
     @Test
     @Owner("Дядя Миша")
-    void textBoxTest()
-    {
+    void textBoxTest() {
 
         String name = faker.name().fullName();
         String email = faker.internet().emailAddress();
         String address1 = faker.address().city();
         String address2 = faker.address().city();
 
-
-
         page
-
                 .openPage()
                 .inputName(name)
                 .inputEmail(email)
@@ -51,7 +42,5 @@ public class TextBoxTest {
                 .inputPermanentAddress(address2)
                 .inputClick()
                 .checkFields(name, email, address1, address2);
-
     }
-
 }
